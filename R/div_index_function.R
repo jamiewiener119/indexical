54e679a9551c67cdf3be3a56075aae13b9b9d1bc
jamiewiener119 <jamie.wiener@utexas.edu>
2024-04-27 17:31:17 -0500

    jamie commit 7

--- R/div_index_function.R
+++ R/div_index_function.R
@@ -1,6 +1,6 @@
 
 #' @title div_index
-#' @description This function allows you explore your census data with several diversity indices (Species Richness, Simpson's Index, Shannon's Index, Berger-Parker Index) and evenness measures (Simpson's Evenness, Shannon's Evenness)
+#' @description This function allows you explore your census data with several diversity indices (Species Richness, Simpson's Index, Shannon's Index, Berger-Parker Index) and evenness measures (Simpson's Evenness, Shannon's Evenness).
 #' @param species_col species_name column from census() function output
 #' @param pop_col individuals column from census() function output
 #' @keywords diversity
@@ -27,7 +27,7 @@ div_index <- function(species_col, pop_col){
   #Simpsons
   for (i in 1:length(species_col)){
     simps_ind[[i]] <- (pop_col[[i]]/tot_pop)^2
-    index_values[[2]] <- sum(simps_ind)
+    index_values[[2]] <- 1-sum(simps_ind)
   }
 
   #Simpson's evenness
